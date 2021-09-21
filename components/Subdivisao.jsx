7e4096d23301c688ffb0a38ff3fe818e54d7f2b3
EnriqueSFlor <enriquee.flor@gmail.com>
2021-09-20 22:56:46 -0300

    primeiro commit do tabuleiro

---
+++ components/Subdivisao.jsx
@@ -0,0 +1,10 @@
+
+import styles from '../styles/Subdivisao.module.css'
+
+export default function Subdivisao(props) {
+    return (
+        <div className={styles.subdivisao}>
+
+        </div>
+    )
+}
